4a4c12f74ebb3c82c0beade51e9ca319cdfa3515
mhmadamrii <interceptorghost4@gmail.com>
2023-03-11 10:54:37 +0700

    add buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,67 @@
 import React from 'react'
 import Image from 'next/image'
 
-import Logo from "#/assets/assets/Logo.png"
+import Logo from '#/assets/assets/Logo.png'
 
 import { Button, Theme, Grid } from '@mui/material'
+import { makeStyles } from '@material-ui/core'
 import { ITheme } from 'interfaces/interface'
-// import { makeStyles } from '@mui/styles'
 
 interface IStyles {
   container: {
-    display: string;
-    position: 'absolute' | 'relative' | 'fixed';
-    top: number;
-  };
+    display: string
+    justifyContent: string
+    position: 'absolute' | 'relative' | 'fixed'
+    top: number
+    right: number
+    left: number
+    padding: string
+    alignItems: string
+    height: number
+  }
+  imageLogo: {
+    width: number
+    height: number
+  }
 }
-
-// const useStyles = makeStyles({
-//   container: {
-//     display: 'flex',
-//     position: 'absolute',
-//     top: 10,
-//   },
-// })
 
 const styles: IStyles = {
   container: {
     display: 'flex',
+    justifyContent: 'space-between',
     position: 'absolute',
     top: 10,
+    right: 0,
+    left: 0,
+    padding: '10px 70px 10px 70px',
+    height: 60,
+    alignItems: 'center',
+    // border: '1px solid red'
+  },
+  imageLogo: {
+    width: 100,
+    height: 50,
   },
 }
 
+const useStyles = makeStyles({
+  registerButton: {
+    backgroundColor: 'black',
+    color: '#FFFF',
+    width: 100,
+    height: 30,
+    fontSize: 10,
+  },
+  loginButton: {
+    border: '1px solid',
+    width: 100,
+    height: 30,
+    fontSize: 10,
+    color: 'black',
+  },
+})
+
 export const NavbarAdmin: React.FC = () => {
-  // const classes = useStyles()
   return (
     <React.Fragment>
       <div style={styles.container}>
@@ -43,17 +72,32 @@ export const NavbarAdmin: React.FC = () => {
 }
 
 export const NavbarUser: React.FC = () => {
+  const classes = useStyles()
   return (
     <React.Fragment>
       <div style={styles.container}>
-
-        <div>
-          <Image src={Logo} alt="Brand Logo" />
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Image src={Logo} alt="Brand Logo" style={styles.imageLogo} />
         </div>
 
-        <div>
-          <Button variant='outlined'>Login</Button>
-          <Button variant='contained'>Register</Button>
+        <div
+          style={{
+            gap: 10,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Button variant="outlined" className={classes.loginButton}>
+            Login
+          </Button>
+          <Button variant="contained" className={classes.registerButton}>
+            Register
+          </Button>
         </div>
       </div>
     </React.Fragment>
